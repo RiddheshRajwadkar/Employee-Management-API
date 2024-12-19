5620b781155d2ae8dce3b6a4781980cceedf3203
RiddheshRajwadkar <riddheshrajwadkar49@gmail.com>
2024-12-19 18:09:38 +0530

    Initial commit

---
+++ src/main/java/com/EmployeeManagement/demo/Employee.java
@@ -0,0 +1,56 @@
+package com.EmployeeManagement.demo;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    @Column(nullable = false)
+    String name;
+    @Column(nullable = false)
+    String department;
+    @Column(nullable = false)
+    double salary;
+
+    public Employee(Long id, String name, String department, double salary) {
+        this.id = id;
+        this.name = name;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
---
+++ src/test/java/com/EmployeeManagement/demo/EmployeeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EmployeeManagement.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
